923a99d2ec681e53f96ad6ef33f9b45c898455df
sleemans <s.leemans@bpm.rwth-aachen.de>
2014-05-09 12:49:22 +0000

    extended colourmap functions
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/GraphViz/Trunk@13413 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/graphviz/colourMaps/ColourMap.java
+++ src/org/processmining/plugins/graphviz/colourMaps/ColourMap.java
@@ -4,6 +4,9 @@ public abstract class ColourMap {
 	public abstract String colour(long weight, long maxWeight);
 	
 	public String colour(long weight, long min, long max) {
+		if (max == min) {
+			return colour(weight, weight);
+		}
 		return colour(weight - min, max - min);
 	}
 }
