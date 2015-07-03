6a6d2797ae73ad4cea3bf6519639c5c8f7bd94cf
sleemans <s.leemans@bpm.rwth-aachen.de>
2015-07-03 15:53:31 +0000

    improved edge-mouse-pointer detection: is now able to detect mouse-on-stroke
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/GraphViz/Trunk@22659 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/com/kitfox/svg/Path.java
+++ src/com/kitfox/svg/Path.java
@@ -1,153 +1,236 @@
 /*
- * SVG Salamander
- * Copyright (c) 2004, Mark McKay
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or 
- * without modification, are permitted provided that the following
- * conditions are met:
- *
- *   - Redistributions of source code must retain the above 
- *     copyright notice, this list of conditions and the following
- *     disclaimer.
- *   - Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials 
- *     provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * SVG Salamander Copyright (c) 2004, Mark McKay All rights reserved.
  * 
- * Mark McKay can be contacted at mark@kitfox.com.  Salamander and other
- * projects can be found at http://www.kitfox.com
- *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. - Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Mark McKay can be contacted at mark@kitfox.com. Salamander and other projects
+ * can be found at http://www.kitfox.com
+ * 
  * Created on January 26, 2004, 5:25 PM
  */
 package com.kitfox.svg;
 
-import com.kitfox.svg.xml.StyleAttribute;
+import java.awt.BasicStroke;
 import java.awt.Graphics2D;
 import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.GeneralPath;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.List;
 
-//import org.apache.batik.ext.awt.geom.ExtendedGeneralPath;
+import com.kitfox.svg.xml.StyleAttribute;
+
+// import org.apache.batik.ext.awt.geom.ExtendedGeneralPath;
 /**
  * @author Mark McKay
  * @author <a href="mailto:mark@kitfox.com">Mark McKay</a>
  */
-public class Path extends ShapeElement
-{
+public class Path extends ShapeElement {
 
-    public static final String TAG_NAME = "path";
-//    PathCommand[] commands = null;
-    int fillRule = GeneralPath.WIND_NON_ZERO;
-    String d = "";
-//    ExtendedGeneralPath path;
-    GeneralPath path;
+	public static final String TAG_NAME = "path";
+	//    PathCommand[] commands = null;
+	int fillRule = GeneralPath.WIND_NON_ZERO;
+	String d = "";
+	//    ExtendedGeneralPath path;
+	GeneralPath path;
 
-    /**
-     * Creates a new instance of Rect
-     */
-    public Path()
-    {
-    }
+	/**
+	 * Creates a new instance of Rect
+	 */
+	public Path() {
+	}
 
-    public String getTagName()
-    {
-        return TAG_NAME;
-    }
+	public String getTagName() {
+		return TAG_NAME;
+	}
 
-    protected void build() throws SVGException
-    {
-        super.build();
+	protected void build() throws SVGException {
+		super.build();
 
-        StyleAttribute sty = new StyleAttribute();
+		StyleAttribute sty = new StyleAttribute();
 
+		String fillRuleStrn = (getStyle(sty.setName("fill-rule"))) ? sty.getStringValue() : "nonzero";
+		fillRule = fillRuleStrn.equals("evenodd") ? GeneralPath.WIND_EVEN_ODD : GeneralPath.WIND_NON_ZERO;
 
-        String fillRuleStrn = (getStyle(sty.setName("fill-rule"))) ? sty.getStringValue() : "nonzero";
-        fillRule = fillRuleStrn.equals("evenodd") ? GeneralPath.WIND_EVEN_ODD : GeneralPath.WIND_NON_ZERO;
+		if (getPres(sty.setName("d"))) {
+			d = sty.getStringValue();
+		}
 
-        if (getPres(sty.setName("d")))
-        {
-            d = sty.getStringValue();
-        }
+		path = buildPath(d, fillRule);
+	}
 
-        path = buildPath(d, fillRule);
-    }
+	//inserted by Sander Leemans
+	@Override
+	void pick(Point2D point, boolean boundingBox, List retVec) throws SVGException {
+		if (boundingBox) {
+			//do nothing special, just call the super function
+			super.pick(point, true, retVec);
+			return;
+		} else {
 
-    public void render(Graphics2D g) throws SVGException
-    {
-        beginLayer(g);
-        renderShape(g, path);
-        finishLayer(g);
-    }
+			//check whether we are on the stroke
+			Shape strokeShape = getStrokeShape(new StyleAttribute(), getShape(), null);
+			if (strokeShape.contains(point)) {
+				//we are on the stroke; call super function to add stuff
+				super.pick(point, true, retVec);
+				return;
+			}
 
-    public Shape getShape()
-    {
-        return shapeToParent(path);
-    }
+			//delegate to super function 
+			super.pick(point, false, retVec);
+			return;
+		}
+	}
 
-    public Rectangle2D getBoundingBox() throws SVGException
-    {
-        return boundsToParent(includeStrokeInBounds(path.getBounds2D()));
-    }
+	//inserted by Sander Leemans
+	public Shape getStrokeShape(StyleAttribute styleAttrib, Shape shape, AffineTransform cacheXform)
+			throws SVGException {
 
-    /**
-     * Updates all attributes in this diagram associated with a time event. Ie,
-     * all attributes with track information.
-     *
-     * @return - true if this node has changed state as a result of the time
-     * update
-     */
-    public boolean updateTime(double curTime) throws SVGException
-    {
-//        if (trackManager.getNumTracks() == 0) return false;
-        boolean changeState = super.updateTime(curTime);
+		float[] strokeDashArray = null;
+		if (getStyle(styleAttrib.setName("stroke-dasharray"))) {
+			strokeDashArray = styleAttrib.getFloatList();
+			if (strokeDashArray.length == 0)
+				strokeDashArray = null;
+		}
 
-        //Get current values for parameters
-        StyleAttribute sty = new StyleAttribute();
-        boolean shapeChange = false;
+		float strokeDashOffset = 0f;
+		if (getStyle(styleAttrib.setName("stroke-dashoffset"))) {
+			strokeDashOffset = styleAttrib.getFloatValueWithUnits();
+		}
 
-        if (getStyle(sty.setName("fill-rule")))
-        {
-            int newVal = sty.getStringValue().equals("evenodd")
-                ? GeneralPath.WIND_EVEN_ODD
-                : GeneralPath.WIND_NON_ZERO;
-            if (newVal != fillRule)
-            {
-                fillRule = newVal;
-                changeState = true;
-            }
-        }
+		int strokeLinecap = BasicStroke.CAP_BUTT;
+		if (getStyle(styleAttrib.setName("stroke-linecap"))) {
+			String val = styleAttrib.getStringValue();
+			if (val.equals("round")) {
+				strokeLinecap = BasicStroke.CAP_ROUND;
+			} else if (val.equals("square")) {
+				strokeLinecap = BasicStroke.CAP_SQUARE;
+			}
+		}
 
-        if (getPres(sty.setName("d")))
-        {
-            String newVal = sty.getStringValue();
-            if (!newVal.equals(d))
-            {
-                d = newVal;
-                shapeChange = true;
-            }
-        }
+		int strokeLinejoin = BasicStroke.JOIN_MITER;
+		if (getStyle(styleAttrib.setName("stroke-linejoin"))) {
+			String val = styleAttrib.getStringValue();
+			if (val.equals("round")) {
+				strokeLinejoin = BasicStroke.JOIN_ROUND;
+			} else if (val.equals("bevel")) {
+				strokeLinejoin = BasicStroke.JOIN_BEVEL;
+			}
+		}
 
-        if (shapeChange)
-        {
-            build();
-//            path = buildPath(d, fillRule);
-//            return true;
-        }
+		float strokeMiterLimit = 4f;
+		if (getStyle(styleAttrib.setName("stroke-miterlimit"))) {
+			strokeMiterLimit = Math.max(styleAttrib.getFloatValueWithUnits(), 1);
+		}
 
-        return changeState || shapeChange;
-    }
+		float strokeOpacity = 1;
+		if (getStyle(styleAttrib.setName("stroke-opacity"))) {
+			strokeOpacity *= styleAttrib.getRatioValue();
+		}
+
+		if (strokeOpacity == 0f) {
+			return null;
+		}
+
+		float strokeWidth = 1f;
+		if (getStyle(styleAttrib.setName("stroke-width"))) {
+			strokeWidth = styleAttrib.getFloatValueWithUnits();
+		}
+		//        if (strokeWidthScalar != 1f)
+		//        {
+		strokeWidth *= strokeWidthScalar;
+		//        }
+
+		BasicStroke stroke;
+		if (strokeDashArray == null) {
+			stroke = new BasicStroke(strokeWidth, strokeLinecap, strokeLinejoin, strokeMiterLimit);
+		} else {
+			stroke = new BasicStroke(strokeWidth, strokeLinecap, strokeLinejoin, strokeMiterLimit, strokeDashArray,
+					strokeDashOffset);
+		}
+
+		Shape strokeShape;
+		if (vectorEffect == VECTOR_EFFECT_NON_SCALING_STROKE) {
+			strokeShape = cacheXform.createTransformedShape(shape);
+			strokeShape = stroke.createStrokedShape(strokeShape);
+		} else {
+			strokeShape = stroke.createStrokedShape(shape);
+		}
+
+		return strokeShape;
+	}
+
+	public void render(Graphics2D g) throws SVGException {
+		beginLayer(g);
+		renderShape(g, path);
+		finishLayer(g);
+	}
+
+	public Shape getShape() {
+		return shapeToParent(path);
+	}
+
+	public Rectangle2D getBoundingBox() throws SVGException {
+		return boundsToParent(includeStrokeInBounds(path.getBounds2D()));
+	}
+
+	/**
+	 * Updates all attributes in this diagram associated with a time event. Ie,
+	 * all attributes with track information.
+	 *
+	 * @return - true if this node has changed state as a result of the time
+	 *         update
+	 */
+	public boolean updateTime(double curTime) throws SVGException {
+		//        if (trackManager.getNumTracks() == 0) return false;
+		boolean changeState = super.updateTime(curTime);
+
+		//Get current values for parameters
+		StyleAttribute sty = new StyleAttribute();
+		boolean shapeChange = false;
+
+		if (getStyle(sty.setName("fill-rule"))) {
+			int newVal = sty.getStringValue().equals("evenodd") ? GeneralPath.WIND_EVEN_ODD : GeneralPath.WIND_NON_ZERO;
+			if (newVal != fillRule) {
+				fillRule = newVal;
+				changeState = true;
+			}
+		}
+
+		if (getPres(sty.setName("d"))) {
+			String newVal = sty.getStringValue();
+			if (!newVal.equals(d)) {
+				d = newVal;
+				shapeChange = true;
+			}
+		}
+
+		if (shapeChange) {
+			build();
+			//            path = buildPath(d, fillRule);
+			//            return true;
+		}
+
+		return changeState || shapeChange;
+	}
 }
