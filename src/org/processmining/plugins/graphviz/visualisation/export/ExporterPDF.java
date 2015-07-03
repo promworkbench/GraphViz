87d58216eb4fb0da5aaa9e1e5254efd76c77e346
sleemans <s.leemans@bpm.rwth-aachen.de>
2015-07-03 10:25:33 +0000

    batik from source
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/GraphViz/Trunk@22622 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/graphviz/visualisation/export/ExporterPDF.java
+++ src/org/processmining/plugins/graphviz/visualisation/export/ExporterPDF.java
@@ -1,7 +1,12 @@
 package org.processmining.plugins.graphviz.visualisation.export;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.fop.svg.PDFTranscoder;
 import org.w3c.dom.svg.SVGDocument;
 
 public class ExporterPDF extends Exporter {
@@ -11,18 +16,18 @@ public class ExporterPDF extends Exporter {
 	}
 
 	public void export(SVGDocument svgDocument, File file) throws Exception {
-//		TranscoderInput input = new TranscoderInput(svgDocument);
-//		input.setURI("svg");
-//		
-//		OutputStream out = new FileOutputStream(file);
-//		TranscoderOutput output = new TranscoderOutput(out);
-//
-//		PDFTranscoder t = new PDFTranscoder();
-//		t.transcode(input, output);
-//
-//        // Flush and close the stream.
-//        out.flush();
-//        out.close();
+		TranscoderInput input = new TranscoderInput(svgDocument);
+		input.setURI("svg");
+		
+		OutputStream out = new FileOutputStream(file);
+		TranscoderOutput output = new TranscoderOutput(out);
+
+		PDFTranscoder t = new PDFTranscoder();
+		t.transcode(input, output);
+
+        // Flush and close the stream.
+        out.flush();
+        out.close();
 	}
 	
 
