fb91b530a3501b44cb2cc671e673ccbba52c17df
sleemans <s.leemans@bpm.rwth-aachen.de>
2016-08-12 08:48:05 +0000

    added pluginlevel=regular
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/GraphViz/Trunk@29612 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/graphviz/visualisation/DotVisualisation.java
+++ src/org/processmining/plugins/graphviz/visualisation/DotVisualisation.java
@@ -5,10 +5,11 @@ import javax.swing.JComponent;
 import org.processmining.contexts.uitopia.annotations.Visualizer;
 import org.processmining.framework.plugin.PluginContext;
 import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.framework.plugin.annotations.PluginLevel;
 import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.plugins.graphviz.dot.Dot;
 
-@Plugin(name = "Dot visualisation", returnLabels = { "Dot visualisation" }, returnTypes = { JComponent.class }, parameterLabels = { "Dot" }, userAccessible = true)
+@Plugin(name = "Dot visualisation", returnLabels = { "Dot visualisation" }, returnTypes = { JComponent.class }, parameterLabels = { "Dot" }, userAccessible = true, level = PluginLevel.Regular)
 @Visualizer
 public class DotVisualisation {
 
